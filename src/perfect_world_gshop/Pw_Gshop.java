deb46dbdafa9b86bc8c3fada8fb2e726d74f063e
GiovaniXX <giovani_chaves@hotmail.com>
2023-03-29 15:14:16 -0300

    Loja de compras de itens em geral Perfect World

---
+++ src/perfect_world_gshop/Pw_Gshop.java
@@ -0,0 +1,281 @@
+package perfect_world_gshop;
+
+import com.formdev.flatlaf.IntelliJTheme;
+
+public class Pw_Gshop extends javax.swing.JFrame {
+
+    public Pw_Gshop() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenuItem11 = new javax.swing.JMenuItem();
+        jMenuItem12 = new javax.swing.JMenuItem();
+        jMenuItem13 = new javax.swing.JMenuItem();
+        jMenuItem14 = new javax.swing.JMenuItem();
+        jMenuItem33 = new javax.swing.JMenuItem();
+        jMenuItem34 = new javax.swing.JMenuItem();
+        jMenuItem15 = new javax.swing.JMenuItem();
+        jMenuItem16 = new javax.swing.JMenuItem();
+        jSeparator2 = new javax.swing.JPopupMenu.Separator();
+        jMenuItem17 = new javax.swing.JMenuItem();
+        jMenuItem18 = new javax.swing.JMenuItem();
+        jMenuItem19 = new javax.swing.JMenuItem();
+        jMenuItem20 = new javax.swing.JMenuItem();
+        jMenuItem21 = new javax.swing.JMenuItem();
+        jMenuItem22 = new javax.swing.JMenuItem();
+        jMenuItem23 = new javax.swing.JMenuItem();
+        jMenuItem24 = new javax.swing.JMenuItem();
+        jMenuItem25 = new javax.swing.JMenuItem();
+        jMenuItem26 = new javax.swing.JMenuItem();
+        jMenuItem27 = new javax.swing.JMenuItem();
+        jMenuItem35 = new javax.swing.JMenuItem();
+        jMenuItem36 = new javax.swing.JMenuItem();
+        jMenuItem28 = new javax.swing.JMenuItem();
+        jMenuItem29 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem30 = new javax.swing.JMenuItem();
+        jMenuItem31 = new javax.swing.JMenuItem();
+        jMenuItem32 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Perfect World Gshop™");
+        setMaximumSize(new java.awt.Dimension(800, 633));
+        setMinimumSize(new java.awt.Dimension(800, 633));
+        setPreferredSize(new java.awt.Dimension(800, 633));
+        setSize(new java.awt.Dimension(800, 633));
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("GENERAL"));
+        jPanel1.setPreferredSize(new java.awt.Dimension(355, 539));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 345, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 516, Short.MAX_VALUE)
+        );
+
+        jMenu1.setText("File");
+
+        jMenuItem2.setText("Load DB");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Save DB");
+        jMenu1.add(jMenuItem3);
+        jMenu1.add(jSeparator1);
+
+        jMenuItem4.setText("Export 1.3.6");
+        jMenu1.add(jMenuItem4);
+
+        jMenuItem5.setText("Export 1.4.2 v27 Cliente");
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem6.setText("Export 1.4.2 v27 Server");
+        jMenu1.add(jMenuItem6);
+
+        jMenuItem7.setText("Export 1.4.2 v29 Cliente");
+        jMenu1.add(jMenuItem7);
+
+        jMenuItem8.setText("Export 1.4.2 v29 Server");
+        jMenu1.add(jMenuItem8);
+
+        jMenuItem9.setText("Export 1.4.4 Cliente");
+        jMenu1.add(jMenuItem9);
+
+        jMenuItem10.setText("Export 1.4.4 Server");
+        jMenu1.add(jMenuItem10);
+
+        jMenuItem11.setText("Export 1.5.2 v127 Cliente");
+        jMenu1.add(jMenuItem11);
+
+        jMenuItem12.setText("Export 1.5.2 v127 Server");
+        jMenu1.add(jMenuItem12);
+
+        jMenuItem13.setText("Export 1.5.3 v145 Cliente");
+        jMenu1.add(jMenuItem13);
+
+        jMenuItem14.setText("Export 1.5.3 v145 Server");
+        jMenu1.add(jMenuItem14);
+
+        jMenuItem33.setText("Export 1.5.5 v156 Cliente");
+        jMenu1.add(jMenuItem33);
+
+        jMenuItem34.setText("Export 1.5.5 v156 Server");
+        jMenu1.add(jMenuItem34);
+
+        jMenuItem15.setText("Export 1.7.2 Cliente");
+        jMenu1.add(jMenuItem15);
+
+        jMenuItem16.setText("Export 1.7.2 Server");
+        jMenu1.add(jMenuItem16);
+        jMenu1.add(jSeparator2);
+
+        jMenuItem17.setText("Import 1.3.6");
+        jMenu1.add(jMenuItem17);
+
+        jMenuItem18.setText("Import 1.4.2 v27 Cliente");
+        jMenu1.add(jMenuItem18);
+
+        jMenuItem19.setText("Import 1.4.2 v27 Server");
+        jMenu1.add(jMenuItem19);
+
+        jMenuItem20.setText("Import 1.4.2 v29 Cliente");
+        jMenu1.add(jMenuItem20);
+
+        jMenuItem21.setText("Import 1.4.2 v29 Server");
+        jMenu1.add(jMenuItem21);
+
+        jMenuItem22.setText("Import 1.4.4 Cliente");
+        jMenu1.add(jMenuItem22);
+
+        jMenuItem23.setText("Import 1.4.4 Server");
+        jMenu1.add(jMenuItem23);
+
+        jMenuItem24.setText("Import 1.5.2 v127 Cliente");
+        jMenu1.add(jMenuItem24);
+
+        jMenuItem25.setText("Import 1.5.2 v127 Server");
+        jMenu1.add(jMenuItem25);
+
+        jMenuItem26.setText("Import 1.5.3 v145 Cliente");
+        jMenu1.add(jMenuItem26);
+
+        jMenuItem27.setText("Import 1.5.3 v145 Server");
+        jMenu1.add(jMenuItem27);
+
+        jMenuItem35.setText("Import 1.5.5 v156 Cliente");
+        jMenu1.add(jMenuItem35);
+
+        jMenuItem36.setText("Import 1.5.5 v156 Server");
+        jMenu1.add(jMenuItem36);
+
+        jMenuItem28.setText("Import 1.7.2 Cliente");
+        jMenu1.add(jMenuItem28);
+
+        jMenuItem29.setText("Import 1.7.2 Server");
+        jMenu1.add(jMenuItem29);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+
+        jMenuItem30.setText("Set All Default Prices 0.01");
+        jMenu2.add(jMenuItem30);
+
+        jMenuItem31.setText("Set All Control Types Cliente");
+        jMenu2.add(jMenuItem31);
+
+        jMenuItem32.setText("Set All Control Types Server");
+        jMenu2.add(jMenuItem32);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Developer");
+
+        jMenuItem1.setText("Info");
+        jMenu3.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu3);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(439, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(65, Short.MAX_VALUE))
+        );
+
+        setSize(new java.awt.Dimension(816, 641));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        try {
+            IntelliJTheme.setup(Pw_Gshop.class.getResourceAsStream("/Visual_Studio_2019_Dark_Theme.theme.json"));
+        } catch (Exception e) {
+        }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Pw_Gshop().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem11;
+    private javax.swing.JMenuItem jMenuItem12;
+    private javax.swing.JMenuItem jMenuItem13;
+    private javax.swing.JMenuItem jMenuItem14;
+    private javax.swing.JMenuItem jMenuItem15;
+    private javax.swing.JMenuItem jMenuItem16;
+    private javax.swing.JMenuItem jMenuItem17;
+    private javax.swing.JMenuItem jMenuItem18;
+    private javax.swing.JMenuItem jMenuItem19;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem20;
+    private javax.swing.JMenuItem jMenuItem21;
+    private javax.swing.JMenuItem jMenuItem22;
+    private javax.swing.JMenuItem jMenuItem23;
+    private javax.swing.JMenuItem jMenuItem24;
+    private javax.swing.JMenuItem jMenuItem25;
+    private javax.swing.JMenuItem jMenuItem26;
+    private javax.swing.JMenuItem jMenuItem27;
+    private javax.swing.JMenuItem jMenuItem28;
+    private javax.swing.JMenuItem jMenuItem29;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem30;
+    private javax.swing.JMenuItem jMenuItem31;
+    private javax.swing.JMenuItem jMenuItem32;
+    private javax.swing.JMenuItem jMenuItem33;
+    private javax.swing.JMenuItem jMenuItem34;
+    private javax.swing.JMenuItem jMenuItem35;
+    private javax.swing.JMenuItem jMenuItem36;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JPopupMenu.Separator jSeparator2;
+    // End of variables declaration//GEN-END:variables
+}
